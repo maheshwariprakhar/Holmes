e1ccc1d7bd9fe71255681b07216112dfa66753bd
Arthi <arthi.sivakumar24@gmail.com>
2018-06-08 09:15:13 -0700

    updates

--- app/src/main/java/com/example/prakharmaheshwari/holmes/EventFeedListFragment.java
+++ app/src/main/java/com/example/prakharmaheshwari/holmes/EventFeedListFragment.java
@@ -134,8 +134,8 @@ public class EventFeedListFragment extends Fragment {
 
                     }
                 }
-                else
-                    isMember = true;
+                //else
+                    //isMember = true;
                 Log.d("people1", Boolean.toString(isMember));
 
                 if(dataSnapshot.hasChild("groupsInvited")) {
@@ -149,8 +149,8 @@ public class EventFeedListFragment extends Fragment {
                         }
                     }
                 }
-                else
-                    ispartofGroup = true;
+                //else
+                //    ispartofGroup = true;
 
                 HEvents hPublicEvents = dataSnapshot.getValue(HEvents.class);
                 Log.d("Events", hPublicEvents.getPr_or_pu());
@@ -167,11 +167,17 @@ public class EventFeedListFragment extends Fragment {
 
 
                     /*Check this*/
-                    if((ispartofGroup && isMember) || User.getDisplayName().equals(host)) {
+                    //if((ispartofGroup || isMember) || User.getDisplayName().equals(host)) {
+
+                    //}
+                    if(!ispartofGroup && (!isMember && !User.getDisplayName().equals(host))) {
 
                     }
-                    else
-                        return;
+                    /*else if(!isMember && (ispartofGroup || User.getDisplayName().equals(host)))
+                    {}
+                    else if(User.getDisplayName().equals(host))
+                    {}*/
+                    else return;
 
                     Log.d("Events2", Boolean.toString(isMember));
                     //if I am part of the group
